13db0f37ec51bd3488ab79fc3f1fac149369d442
Sunghyun627 <kksscc4@gmail.com>
2024-02-24 18:47:26 +0900

    feat: 프로젝트 전체 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import Layout from './components/layout/Layout';
-import Home from './pages/Home';
 import { BookStoreThemeProvider } from './context/themeContext';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Error from './components/common/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+    errorElement: <Error />,
+  },
+  {
+    path: '/books',
+    element: (
+      <Layout>
+        <div>도서 목록</div>
+      </Layout>
+    ),
+  },
+]);
 
 const App = () => {
   return (
     <BookStoreThemeProvider>
-      <Layout>Home</Layout>
+      <RouterProvider router={router} />
     </BookStoreThemeProvider>
   );
 };
